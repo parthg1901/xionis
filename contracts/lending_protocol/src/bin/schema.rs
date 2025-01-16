3ea9fa0b8fc2059b58a5086613efbd6fc283fba7
Sahilgill24 <gursahilgill24@gmail.com>
2025-01-17 02:56:48 +0530

    contracts layout setup

---
+++ contracts/lending_protocol/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use lending_protocol::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
