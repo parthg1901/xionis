6523cf481a0dc5ee408719dc7f7b676c35e09212
Parth Gupta <parth.eng1210@gmail.com>
2025-01-18 06:51:42 +0530

    update treasury

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,9 @@ const jersey = Jersey_15({
   variable: "--font-jersey",
 });
 
-// const treasuryConfig = {
-//   // treasury: "xion1z70cvc08qv5764zeg3dykcyymj5z6nu4sqr7x8vl4zjef2gyp69s9mmdka",
-// };
+const treasuryConfig = {
+  treasury: "xion1cp43h483qnqgayqm8mn8lavc74rpst077tu9xq2qk9rua50zhvhshmsfhq",
+};
 
 export default function RootLayout({
   children,
@@ -27,8 +27,9 @@ export default function RootLayout({
       <body className="font-[family-name:var(--font-inter)]">
         <AbstraxionProvider
           config={{
+            treasury: treasuryConfig.treasury,
             contracts: [
-              "xion1z70cvc08qv5764zeg3dykcyymj5z6nu4sqr7x8vl4zjef2gyp69s9mmdka",
+              "xion1hy7p8aq7nlvg2j4v57z5dlwtvvz7awz2wl0sx3d3qrfelt99z8uqd9p6er",
             ],
           }}
         >
